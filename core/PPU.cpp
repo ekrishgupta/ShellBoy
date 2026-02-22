6c3a24362d41c4665b869ef6ebeb603500e19791
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-21 21:06:42 -0800

    feat(ppu): implement read/write for LCD registers

--- core/PPU.cpp
+++ core/PPU.cpp
@@ -39,3 +39,67 @@ uint8_t PPU::read(uint16_t address) const { return vram[address - 0x8000]; }
 void PPU::write(uint16_t address, uint8_t value) {
   vram[address - 0x8000] = value;
 }
+
+uint8_t PPU::readReg(uint16_t address) const {
+  switch (address) {
+  case 0xFF40:
+    return lcdc;
+  case 0xFF41:
+    return stat;
+  case 0xFF42:
+    return scy;
+  case 0xFF43:
+    return scx;
+  case 0xFF44:
+    return currentScanline;
+  case 0xFF45:
+    return lyc;
+  case 0xFF47:
+    return bgp;
+  case 0xFF48:
+    return obp0;
+  case 0xFF49:
+    return obp1;
+  case 0xFF4A:
+    return wy;
+  case 0xFF4B:
+    return wx;
+  default:
+    return 0xFF; // Unmapped PPU registers return 0xFF
+  }
+}
+
+void PPU::writeReg(uint16_t address, uint8_t value) {
+  switch (address) {
+  case 0xFF40:
+    lcdc = value;
+    break;
+  case 0xFF41:
+    stat = value;
+    break;
+  case 0xFF42:
+    scy = value;
+    break;
+  case 0xFF43:
+    scx = value;
+    break;
+  case 0xFF45:
+    lyc = value;
+    break;
+  case 0xFF47:
+    bgp = value;
+    break;
+  case 0xFF48:
+    obp0 = value;
+    break;
+  case 0xFF49:
+    obp1 = value;
+    break;
+  case 0xFF4A:
+    wy = value;
+    break;
+  case 0xFF4B:
+    wx = value;
+    break;
+  }
+}
