b723fb5bac4ad9ab244f303c43607442007d8b40
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-02-21 20:57:23 -0800

    feat(mmu): implement ROM loading and basic read

--- mmu/Cartridge.cpp
+++ mmu/Cartridge.cpp
@@ -1,15 +1,28 @@
 #include "Cartridge.h"
+#include <fstream>
 
 Cartridge::Cartridge() {}
 Cartridge::~Cartridge() {}
 
 bool Cartridge::loadRom(const std::string &filepath) {
-  // Stub
-  return true;
+  std::ifstream file(filepath, std::ios::binary | std::ios::ate);
+  if (!file.is_open())
+    return false;
+
+  std::streamsize size = file.tellg();
+  file.seekg(0, std::ios::beg);
+
+  rom.resize(size);
+  if (file.read(reinterpret_cast<char *>(rom.data()), size)) {
+    return true;
+  }
+
+  return false;
 }
 
 uint8_t Cartridge::read(uint16_t address) const {
-  // Stub
+  if (address < rom.size())
+    return rom[address];
   return 0xFF;
 }
 
