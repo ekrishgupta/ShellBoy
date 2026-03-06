4b9d284683bc78e4abc943fcf3385f07af2ba282
ekrishgupta <ekrishgupta@users.noreply.github.com>
2026-03-05 18:56:00 -0800

    fix(ppu): initialize BGP palette to 0xFC for post-boot state

--- core/PPU.cpp
+++ core/PPU.cpp
@@ -1,6 +1,6 @@
 #include "PPU.h"
 
-PPU::PPU(Bus &b) : bus(b) { frameBuffer.fill(0); lcdc = 0x91; stat = 0x85; }
+PPU::PPU(Bus &b) : bus(b) { frameBuffer.fill(0); lcdc = 0x91; stat = 0x85; bgp = 0xFC; }
 
 PPU::~PPU() {}
 
